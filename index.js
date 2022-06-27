969aadc68fa32fbc9e6a46b62c7abaecd3a356b7
Dj2035 <dk.jude@yahoo.com>
2022-06-27 16:28:36 +0200

    Add Express code for all endpoints in api

--- index.js
+++ index.js
@@ -1,49 +1,86 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
 const app = express();
 
-//List of top 10 movies
-let topMovies = [
+app.use(bodyParser.json());
+
+//List of movies
+let movies = [
   {
-    title: 'Aquaman',
-    actor: 'Jason Momoa'
+    Title: "Aquaman",
+    Actor: "Jason Momoa",
+    Genre: {
+      Name: "Drama",
+      Description: "Drama is category of narrative fiction"
+    },
+    Director: {
+      Name: "Darren Aronofsky",
+      Bio: "Born february 12, 1969"
+    }
   },
   {
-    title: 'Black Panther',
-    actor: 'Chadwick Boseman'
+    Title: "Black Panther",
+    Actor: "Chadwick Boseman",
+    Genre: {
+      Name: "Action",
+      Description: "Action is category of fiction with many fighting scenes"
+    },
+    Director: {
+      Name: "Arnold Scheider",
+      Bio: "Born february 12, 1975"
+    }
   },
   {
-    title: 'Spider man',
-    actor: 'Tom Holland'
+    Title: "Walking Dead",
+    Actor: "Nick Peterson",
+    Genre: {
+      Name: "Horor",
+      Description: "Horor is category of frightful movies"
+    },
+    Director: {
+      Name: "Luc Sonderland",
+      Bio: "Born May 12, 1970"
+    }
   },
   {
-    title: 'Fast Five',
-    actor: 'Vin Diesel'
+    Title: "John Wick",
+    Actor: "Keanu Reeves",
+    Genre: {
+      Name: "Action",
+      Description: "Action is category of fiction with many fighting scenes"
+    },
+    Director: {
+      Name: "Morgan Freemann",
+      Bio: "Born November 19, 1976"
+    }
   },
   {
-    title: 'John Wick',
-    actor: 'Keanu Reeves'
+    Title: "The Princess Bride",
+    Actor: "Martin Talisman",
+    Genre: {
+      Name: "Action",
+      Description: "Action is category of fiction with many fighting scenes"
+    },
+    Director: {
+      Name: "Ron Reiner",
+      Bio: "Born february 12, 1975"
+    }
+  }
+];
+
+let users = [
+  {
+    id: 1,
+    name: "Kim",
+    favoriteMovies: []
   },
   {
-    title: 'Terminator',
-    actor: 'Arnold Schwarzenegger'
-  },
-  {
-    title: 'Ninja Assassins',
-    actor: 'Rain Lee Joon'
-  },
-  {
-    title: 'Mission impossible',
-    actor: 'Tom Cruise'
-  },
-  {
-    title: 'Wonder Woman',
-    actor: 'Gal Gadot'
-  },
-  {
-    title: 'Taken',
-    actor: 'Liam Neeson'
+    id: 2,
+    name: "Joe",
+    favoriteMovies: ["Aquaman"]
   }
 ];
 
@@ -53,14 +90,120 @@ app.use(morgan('common'));
 //Serving static documentation file located in public folder
 app.use('/', express.static('public'));
 
-//request function for top 10 movies
+//GET movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(movies);
 });
 
-//Default textual response for root folder
-app.get('/', (req, res) => {
-  res.send('Welcome to my movie api!');
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
+});
+
+//GET single movie title
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.title === title );
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie');
+  }
+});
+
+//GET data about genre by name
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = Movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre');
+  }
+});
+
+//GET data about director by name
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = Movies.find( movie => movie.Director.Name === directorName ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director');
+  }
+});
+
+//Post: Allow users to register
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+});
+
+//UPDATE user name
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+//CREATE: Allow users add favorite movies
+app.post('/users/:id:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  let user = users.find(user => user.id == id);
+
+  if(user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array'`);
+  } else {
+    res.status(400).send('no such user')
+  }
+
+});
+
+//DELETE a movie by title
+app.delete('/users/:id:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array'`);
+  } else {
+    res.status(400).send('no such user')
+  }
+});
+
+
+//DELETE user
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    users = users.filter( user.id != id );
+    res.status(200).send(`user ${id} has been successfully deleted'`);
+  } else {
+    res.status(400).send('no such user')
+  }
 });
 
 app.use((err, req, res, next) => {
