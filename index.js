929f7c351d69cf17504f004e1af65ea84641528f
Dj2035 <dk.jude@yahoo.com>
2022-06-29 10:29:37 +0200

    Fix bug on Express code for POST and DELETE requests

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let movies = [
   {
     Title: "Aquaman",
     Actor: "Jason Momoa",
+    movieID: "1",
     Genre: {
       Name: "Drama",
       Description: "Drama is category of narrative fiction"
@@ -24,6 +25,7 @@ let movies = [
   {
     Title: "Black Panther",
     Actor: "Chadwick Boseman",
+    movieID: "2",
     Genre: {
       Name: "Action",
       Description: "Action is category of fiction with many fighting scenes"
@@ -36,9 +38,10 @@ let movies = [
   {
     Title: "Walking Dead",
     Actor: "Nick Peterson",
+    movieID: "3",
     Genre: {
-      Name: "Horor",
-      Description: "Horor is category of frightful movies"
+      Name: "Horror",
+      Description: "Horror is category of frightful movies"
     },
     Director: {
       Name: "Luc Sonderland",
@@ -48,6 +51,7 @@ let movies = [
   {
     Title: "John Wick",
     Actor: "Keanu Reeves",
+    movieID: "4",
     Genre: {
       Name: "Action",
       Description: "Action is category of fiction with many fighting scenes"
@@ -60,6 +64,7 @@ let movies = [
   {
     Title: "The Princess Bride",
     Actor: "Martin Talisman",
+    movieID: "5",
     Genre: {
       Name: "Action",
       Description: "Action is category of fiction with many fighting scenes"
@@ -73,13 +78,13 @@ let movies = [
 
 let users = [
   {
-    id: 1,
-    name: "Kim",
+    id: "1",
+    username: "Kim",
     favoriteMovies: []
   },
   {
-    id: 2,
-    name: "Joe",
+    id: "2",
+    username: "Joe",
     favoriteMovies: ["Aquaman"]
   }
 ];
@@ -92,17 +97,17 @@ app.use('/', express.static('public'));
 
 //GET movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  res.status(200).send('Successful GET request returning data on all the movies');
 });
 
 app.get('/users', (req, res) => {
-  res.status(200).json(users);
+  res.status(200).json('movies');
 });
 
 //GET single movie title
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
-  const movie = movies.find( movie => movie.title === title );
+  const movie = movies.find( movie => movie.Title === title );
 
   if (movie) {
     res.status(200).json(movie);
@@ -114,7 +119,7 @@ app.get('/movies/:title', (req, res) => {
 //GET data about genre by name
 app.get('/movies/genre/:genreName', (req, res) => {
   const { genreName } = req.params;
-  const genre = Movies.find( movie => movie.Genre.Name === genreName ).Genre;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
 
   if (genre) {
     res.status(200).json(genre);
@@ -126,7 +131,7 @@ app.get('/movies/genre/:genreName', (req, res) => {
 //GET data about director by name
 app.get('/movies/directors/:directorName', (req, res) => {
   const { directorName } = req.params;
-  const director = Movies.find( movie => movie.Director.Name === directorName ).Director;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
 
   if (director) {
     res.status(200).json(director);
@@ -139,12 +144,12 @@ app.get('/movies/directors/:directorName', (req, res) => {
 app.post('/users', (req, res) => {
   const newUser = req.body;
 
-  if (newUser.name) {
+  if (newUser.username) {
     newUser.id = uuid.v4();
     users.push(newUser);
     res.status(201).json(newUser)
   } else {
-    res.status(400).send('users need names')
+    res.status(400).send('users need usernames')
   }
 });
 
@@ -156,7 +161,7 @@ app.put('/users/:id', (req, res) => {
   let user = users.find( user => user.id == id );
 
   if (user) {
-    user.name = updatedUser.name;
+    user.username = updatedUser.username;
     res.status(200).json(user);
   } else {
     res.status(400).send('no such user')
@@ -164,13 +169,13 @@ app.put('/users/:id', (req, res) => {
 });
 
 //CREATE: Allow users add favorite movies
-app.post('/users/:id:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  let user = users.find(user => user.id == id);
+app.post('/users/:username/movies/:movieId', (req, res) => {
+  const { username, movieId } = req.params;
+  let user = users.find( user => user.username == username );
 
   if(user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array'`);
+    user.favoriteMovies.push(movieId);
+    res.status(200).send(`Movie with id: ${movieId} has been added to ${username}'s array'`);
   } else {
     res.status(400).send('no such user')
   }
@@ -178,14 +183,14 @@ app.post('/users/:id:movieTitle', (req, res) => {
 });
 
 //DELETE a movie by title
-app.delete('/users/:id:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+app.delete('/users/:username/movies/:movieId', (req, res) => {
+  const { username, movieId } = req.params;
 
-  let user = users.find( user => user.id == id );
+  let user = users.find( user => user.username == username );
 
   if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array'`);
+    user.favoriteMovies = user.favoriteMovies.filter( movie => movie !== movieId);
+    res.status(200).send(`Movie with id: ${movieId} has been removed from ${username}'s array'`);
   } else {
     res.status(400).send('no such user')
   }
@@ -199,16 +204,11 @@ app.delete('/users/:id', (req, res) => {
   let user = users.find( user => user.id == id );
 
   if (user) {
-    users = users.filter( user.id != id );
-    res.status(200).send(`user ${id} has been successfully deleted'`);
+    users = users.filter( user => user.id != id );
+    res.status(200).send(`user with id: ${id} has been successfully deleted`);
   } else {
     res.status(400).send('no such user')
   }
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
 });
 
 app.listen(8080, () => {
