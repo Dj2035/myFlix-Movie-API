57686319bce6d4e8309eded4db00cb58f2efe621
Dj2035 <dk.jude@yahoo.com>
2022-07-05 19:50:17 +0200

    Add models.js file and create models for Movie schema and User schema using Mongoose

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Actors: [String],
+  Genre: {
+    Name: String,
+    Descripton: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: String,
+    Death: String
+  },
+  ReleaseYear: String,
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
