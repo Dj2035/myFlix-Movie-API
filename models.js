8877a8e102d51f4116d0180fe557b511a7c46046
Dj2035 <dk.jude@yahoo.com>
2022-07-11 13:10:01 +0200

    Correct mistake on hashed password comparison during login

--- models.js
+++ models.js
@@ -33,7 +33,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
