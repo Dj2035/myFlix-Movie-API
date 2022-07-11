66155117fb8cea1cddd5d80278ffd4e2e952c9b9
Dj2035 <dk.jude@yahoo.com>
2022-07-11 13:09:20 +0200

    Update index.js to allow all domains to make requests to API

--- index.js
+++ index.js
@@ -20,18 +20,7 @@ const { check, validationResult } = require('express-validator');
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does\'nt allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth')(app);
 
