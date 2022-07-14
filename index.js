22ae6f1213ee0729cd2f6794fc2efb040b97a225
Dj2035 <dk.jude@yahoo.com>
2022-07-14 08:13:46 +0200

    Add code for hashing password when user updates info

--- index.js
+++ index.js
@@ -170,11 +170,12 @@ passport.authenticate('jwt', { session: false }),
     return res.status(422).json({ errors: errors.array() });
   }
 
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username },
     {
       $set: {
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
     }
