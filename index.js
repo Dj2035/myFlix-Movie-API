433ec384a79e36d187412c31be964afebcf2824d
Dj2035 <dk.jude@yahoo.com>
2022-07-23 19:55:47 +0200

    Temporarily disabled passport authentification for movies endpoint

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 
 
 //GET all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
